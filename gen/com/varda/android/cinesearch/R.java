429d4aa85e7c3b8208279ff26ce1b92185383e50
pvardanega <pvardanega@xebia.fr>
2010-12-31 00:09:25 +0100

    Customizing movies list and movie info.

--- gen/com/varda/android/cinesearch/R.java
+++ gen/com/varda/android/cinesearch/R.java
@@ -10,6 +10,9 @@ package com.varda.android.cinesearch;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int blue=0x7f040000;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
         public static final int logo=0x7f020001;
@@ -18,20 +21,20 @@ public final class R {
         public static final int search=0x7f020004;
     }
     public static final class id {
-        public static final int movie_description=0x7f05000d;
-        public static final int movie_lang=0x7f05000c;
-        public static final int movie_name=0x7f050009;
-        public static final int movie_poster=0x7f050008;
-        public static final int movie_rate=0x7f05000a;
-        public static final int movie_release_date=0x7f05000b;
-        public static final int movie_row_name=0x7f050006;
-        public static final int movie_row_release=0x7f050007;
-        public static final int movie_search_radio_button=0x7f050002;
-        public static final int people_search_radio_button=0x7f050003;
-        public static final int reset_button=0x7f050005;
-        public static final int search_button=0x7f050004;
-        public static final int search_edit_text=0x7f050000;
-        public static final int search_radio_group=0x7f050001;
+        public static final int movie_description=0x7f06000d;
+        public static final int movie_lang=0x7f06000c;
+        public static final int movie_name=0x7f060009;
+        public static final int movie_poster=0x7f060008;
+        public static final int movie_rate=0x7f06000a;
+        public static final int movie_release_date=0x7f06000b;
+        public static final int movie_row_name=0x7f060006;
+        public static final int movie_row_release=0x7f060007;
+        public static final int movie_search_radio_button=0x7f060002;
+        public static final int people_search_radio_button=0x7f060003;
+        public static final int reset_button=0x7f060005;
+        public static final int search_button=0x7f060004;
+        public static final int search_edit_text=0x7f060000;
+        public static final int search_radio_group=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -40,12 +43,12 @@ public final class R {
         public static final int movies_layout=0x7f030003;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int app_short_desc=0x7f040005;
-        public static final int clear_search=0x7f040004;
-        public static final int movies=0x7f040002;
-        public static final int no_movie_found=0x7f040006;
-        public static final int people=0x7f040003;
-        public static final int search=0x7f040001;
+        public static final int app_name=0x7f050000;
+        public static final int app_short_desc=0x7f050005;
+        public static final int clear_search=0x7f050004;
+        public static final int movies=0x7f050002;
+        public static final int no_movie_found=0x7f050006;
+        public static final int people=0x7f050003;
+        public static final int search=0x7f050001;
     }
 }
