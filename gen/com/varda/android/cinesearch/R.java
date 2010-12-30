302f003612b111796e26135e93668161b3014bbc
pvardanega <pvardanega@xebia.fr>
2010-12-30 11:44:23 +0100

    Creating the search form.

--- gen/com/varda/android/cinesearch/R.java
+++ gen/com/varda/android/cinesearch/R.java
@@ -13,4 +13,22 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int movie_search_radio_button=0x7f050002;
+        public static final int people_search_radio_button=0x7f050003;
+        public static final int search_button=0x7f050005;
+        public static final int search_edit_text=0x7f050000;
+        public static final int search_radio_group=0x7f050001;
+        public static final int search_type_text_view=0x7f050004;
+    }
+    public static final class layout {
+        public static final int main=0x7f030000;
+    }
+    public static final class string {
+        public static final int app_name=0x7f040000;
+        public static final int app_title=0x7f040001;
+        public static final int movies=0x7f040003;
+        public static final int people=0x7f040004;
+        public static final int search=0x7f040002;
+    }
 }
--- src/com/varda/android/cinesearch/SearchActivity.java
+++ src/com/varda/android/cinesearch/SearchActivity.java
@@ -1,6 +1,13 @@
 package com.varda.android.cinesearch;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class SearchActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
 }
