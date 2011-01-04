0a8d290b2cc341e9b0a12b51ca72d203c86ea6fe
pvardanega <pvardanega@xebia.fr>
2011-01-04 16:23:44 +0100

    Customizing movie UI by adding stars rating.

--- gen/com/varda/android/cinesearch/R.java
+++ gen/com/varda/android/cinesearch/R.java
@@ -19,21 +19,28 @@ public final class R {
         public static final int movie_icon=0x7f020002;
         public static final int people_icon=0x7f020003;
         public static final int search=0x7f020004;
+        public static final int star1=0x7f020005;
+        public static final int star2=0x7f020006;
+        public static final int star3=0x7f020007;
+        public static final int star4=0x7f020008;
+        public static final int star5=0x7f020009;
     }
     public static final class id {
-        public static final int movie_description=0x7f06000e;
-        public static final int movie_lang=0x7f06000d;
+        public static final int movie_description=0x7f060010;
+        public static final int movie_lang=0x7f06000f;
         public static final int movie_name=0x7f06000a;
         public static final int movie_poster=0x7f060009;
-        public static final int movie_rate=0x7f06000b;
-        public static final int movie_release_date=0x7f06000c;
+        public static final int movie_rate=0x7f06000d;
+        public static final int movie_rate_prefix=0x7f06000b;
+        public static final int movie_rate_stars=0x7f06000c;
+        public static final int movie_release_date=0x7f06000e;
         public static final int movie_row_name=0x7f060007;
         public static final int movie_row_release=0x7f060008;
         public static final int movie_row_thumb=0x7f060006;
         public static final int movie_search_radio_button=0x7f060002;
         public static final int people_search_radio_button=0x7f060003;
-        public static final int person_name=0x7f060010;
-        public static final int person_poster=0x7f06000f;
+        public static final int person_name=0x7f060012;
+        public static final int person_poster=0x7f060011;
         public static final int reset_button=0x7f060005;
         public static final int search_button=0x7f060004;
         public static final int search_edit_text=0x7f060000;
@@ -52,10 +59,13 @@ public final class R {
         public static final int app_short_desc=0x7f050005;
         public static final int clear_search=0x7f050004;
         public static final int date_source_format=0x7f050008;
+        public static final int language=0x7f050009;
         public static final int movies=0x7f050002;
         public static final int no_movie_found=0x7f050006;
         public static final int no_person_found=0x7f050007;
         public static final int people=0x7f050003;
+        public static final int rating=0x7f05000b;
+        public static final int release_date=0x7f05000a;
         public static final int search=0x7f050001;
     }
 }
--- src/com/varda/android/cinesearch/resultsform/MovieInfoActivity.java
+++ src/com/varda/android/cinesearch/resultsform/MovieInfoActivity.java
@@ -1,12 +1,14 @@
 package com.varda.android.cinesearch.resultsform;
 
 import android.app.Activity;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.varda.android.cinesearch.R;
-import com.varda.android.cinesearch.httpsearch.HttpRetriever;
 import com.varda.android.cinesearch.resultsform.adapters.BitmapDownloaderTask;
 import com.varda.android.cinesearch.xmlbiddings.Movie;
 
@@ -25,7 +27,8 @@ public class MovieInfoActivity extends Activity {
     private TextView ratingTextView;
     private TextView releaseTextView;
     private TextView languageTextView;
-    private ImageView imageView;
+    private ImageView posterImageView;
+    private ImageView ratingImageView;
     private TextView descriptionTextView;
 
     private static Locale[] locales = Locale.getAvailableLocales();
@@ -48,7 +51,9 @@ public class MovieInfoActivity extends Activity {
 
         // Rating
         this.ratingTextView = (TextView) this.findViewById(R.id.movie_rate);
-        this.ratingTextView.setText("Rating: " + movie.rating);
+        this.ratingTextView.setText(" (" + movie.rating + ")");
+        this.ratingImageView = (ImageView) this.findViewById(R.id.movie_rate_stars);
+        this.setStarToButtons();
 
         // Release
         this.releaseTextView = (TextView) this.findViewById(R.id.movie_release_date);
@@ -56,16 +61,16 @@ public class MovieInfoActivity extends Activity {
         SimpleDateFormat sourceSdf = new SimpleDateFormat(this.getString(R.string.date_source_format));
         try {
             Date releaseDate = sourceSdf.parse(movie.released);
-            this.releaseTextView.setText("Release date: " + sdf.format(releaseDate));
+            this.releaseTextView.setText(getString(R.string.release_date)+ " " + sdf.format(releaseDate));
         } catch (ParseException e) {
             Log.w(getClass().getSimpleName(), "Error while parsing date " + movie.released, e);
-            this.releaseTextView.setText("Release date: " + movie.released);
+            this.releaseTextView.setText(getString(R.string.release_date)+ " " + movie.released);
         }
 
         // Language
         this.languageTextView = (TextView) this.findViewById(R.id.movie_lang);
         StringBuilder language = new StringBuilder();
-        language.append("Language: ");
+        language.append(getString(R.string.language) + " ");
         Locale locale = getLocaleFromLanguage(movie.language);
         StringBuilder localeCountryName = new StringBuilder(locale.getDisplayName());
         if (locale == null || "".equals(localeCountryName)) {
@@ -77,17 +82,33 @@ public class MovieInfoActivity extends Activity {
         this.languageTextView.setText(language);
 
         // Thumb image
-        this.imageView = (ImageView) this.findViewById(R.id.movie_poster);
+        this.posterImageView = (ImageView) this.findViewById(R.id.movie_poster);
         this.posterUrl = movie.retrieveThumbnail();
         if (this.posterUrl == null) {
-            this.imageView.setImageBitmap(null);
+            this.posterImageView.setImageBitmap(null);
         } else {
-            new BitmapDownloaderTask(this.imageView).execute(this.posterUrl);
+            new BitmapDownloaderTask(this.posterImageView).execute(this.posterUrl);
         }
 
         // Description
         this.descriptionTextView = (TextView) this.findViewById(R.id.movie_description);
         this.descriptionTextView.setText(movie.overview);
+    }
+
+    private void setStarToButtons() {
+        double rate = Double.valueOf(movie.rating) / 2.0;
+
+        if (rate >= 0.5 && rate < 1.5) {
+            this.ratingImageView.setImageResource(R.drawable.star1);
+        } else if (rate >= 1.5 && rate < 2.5) {
+            this.ratingImageView.setImageResource(R.drawable.star2);
+        } else if (rate >= 2.5 && rate < 3.5) {
+            this.ratingImageView.setImageResource(R.drawable.star3);
+        } else if (rate >= 3.5 && rate < 4.5) {
+            this.ratingImageView.setImageResource(R.drawable.star4);
+        } else if (rate >= 4.5) {
+            this.ratingImageView.setImageResource(R.drawable.star5);
+        }
     }
 
     private Locale getLocaleFromLanguage(String lang) {
